6b1d816739aae05d108369d1547208207ea13c67
thequinneffect <n_quinny12@hotmail.com>
2023-09-18 14:02:03 +1000

    core model functionality implemented and tested
    * added the functionality to calculate which weight plates should be used to hit a desired target weight
      * requires dynamic programming since greedy can return non-optimal solution or claim there is no solution when there is a solution
        * non-optimal: weights = {25, 20, 20, 10, 5} target = 40, greedy takes 25 + 10 + 5 (3 plates) when you can do 20 + 20 (2 plates)
        * false no solution claim: weights = {25, 20, 20, 10} target = 40, greedy takes 25 + 10 then fails and claims no solution, but 20 + 20 exists
      * this is just the coin change problem except it has 2 quirks:
        * the need to upscale the doubles to integers
        * the need to save the actual weights (i.e. coins) used to make the target weight (i.e. target money)
    * unit tested the behaviour and it seems to work
    * still need to implement some other methods later in order to support user interactions i.e. updating the weight plate set

---
+++ BarbellWeightCalculator/BarbellWeightCalculator/Model.cs
@@ -0,0 +1,152 @@
+﻿using Avalonia.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace BarbellWeightCalculator
+{
+
+    public class WeightConfiguration
+    {
+        const byte BARBELL_SIDES = 2;
+        // Only support 3 decimal places of precision for weight values e.g. 0.125kg
+        readonly uint WEIGHT_SCALER = 1000;
+
+        private double _targetWeight;
+
+        public double TargetWeight { 
+            get => _targetWeight;
+            set
+            {
+                if (value < 0 || value < BarbellWeight || value == _targetWeight
+                    || value > MaxTargetWeight) return;
+                _targetWeight = value;
+            }
+        }
+
+        public static double MaxTargetWeight => 2000;
+
+        private double _barbellWeight;
+
+        public double BarbellWeight { 
+            get => _barbellWeight; 
+            set 
+            {
+                if (value < 0 || value == _barbellWeight) return;
+                _barbellWeight = value;
+            }
+        }
+
+        public bool IsMetric { get; set; }
+
+        // plate -> quantity e.g. 25 -> 2 means we have 2x 25 kg|lb plates
+        // storing double for plate key since it makes sorting easy and double for
+        // value since plates can have fractional weights e.g. a 2.5 kg plate.
+        // Storing plates per side since we load the same on each side. This does
+        // assume that the person has pairs of each plate, but that is a reasonable
+        // assumption.
+        public SortedDictionary<double, uint> PlatesPerSide { get; set; }
+
+        public WeightConfiguration(double targetWeight, double barbellWeight, bool isMetric, 
+            SortedDictionary<double, uint> platesPerSide)
+        {
+            TargetWeight = targetWeight;
+            BarbellWeight = barbellWeight;
+            IsMetric = isMetric;
+            PlatesPerSide = platesPerSide;
+        }
+
+        private double CalculateSingleSidePlateWeight() 
+            => (TargetWeight - BarbellWeight) / BARBELL_SIDES;
+
+
+        /// <summary>
+        /// Uses dynamic programming to calculate the plates required to hit a target
+        /// weight. Returns an empty list if not possible.
+        /// </summary>
+        /// <param name="plates"> The list of plate weights available per side.
+        /// e.g. 25, 25, 20, 20, 20, 20, 15, 10, 2.5, 1 </param>
+        /// <param name="targetWeight"> The target weight to make from the plates. </param>
+        /// <returns></returns>
+        private List<double> FindMinimumWeights(double[] plates, double targetWeight)
+        {
+            int n = plates.Length;
+            int[] memo = new int[(int)(targetWeight * WEIGHT_SCALER) + 1];
+            int[] lastWeightIndex = new int[(int)(targetWeight * WEIGHT_SCALER) + 1];
+
+            int intTargetWeight = (int)(targetWeight * WEIGHT_SCALER);
+
+            // Initialize memo with a large value (infinite) to represent no solution
+            for (int i = 1; i <= intTargetWeight; i++)
+            {
+                memo[i] = int.MaxValue;
+            }
+
+            // Set the base case
+            memo[0] = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = intTargetWeight; j >= 0; j--)
+                {
+                    if (memo[j] != int.MaxValue && j + (int)(plates[i] * WEIGHT_SCALER) <= intTargetWeight)
+                    {
+                        int nextWeight = j + (int)(plates[i] * WEIGHT_SCALER);
+                        // <= since we want to take largest plates possible
+                        if (memo[j] + 1 <= memo[nextWeight])
+                        {
+                            memo[nextWeight] = memo[j] + 1;
+                            lastWeightIndex[nextWeight] = i;
+                        }
+                    }
+                }
+            }
+
+            if (memo[intTargetWeight] == int.MaxValue)
+            {
+                return new List<double>(); // No solution
+            }
+
+            // Reconstruct the minimum weight subset
+            List<double> selectedWeights = new List<double>();
+            int remainingWeight = intTargetWeight;
+            while (remainingWeight > 0)
+            {
+                int lastWeightIdx = lastWeightIndex[remainingWeight];
+                selectedWeights.Add(plates[lastWeightIdx]);
+                remainingWeight -= (int)(plates[lastWeightIdx] * WEIGHT_SCALER);
+            }
+
+            return selectedWeights;
+        }
+
+        /// <summary>
+        /// Calculates the set of weight plates you need to put on each side of the barbell
+        /// in order to achieve the target weight (or empty set if not possible).
+        /// The set of weights is guaranteed to be the minimal set i.e. there may be other
+        /// ways to achieve the target weight, but they use the same amount or more plates.
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<double, uint> CalculatePlateSet()
+        {
+            // get flat array of weight plates i.e. dictionary of 25kg -> 2, 10kg -> 1
+            // would become 25, 25, 10
+            double[] plates = PlatesPerSide.Reverse().SelectMany(
+                pair => Enumerable.Repeat(pair.Key, (int)pair.Value)).ToArray();
+
+            double remainingWeight = CalculateSingleSidePlateWeight();
+
+            var answer = FindMinimumWeights(plates, remainingWeight);
+
+            var plateSet = new Dictionary<double, uint>();
+            foreach (double plate in answer)
+            {
+                plateSet.TryGetValue(plate, out uint plateCount);
+                plateSet[plate] = plateCount + 1;
+            }
+            return plateSet;
+        }
+
+    }
+}
--- BarbellWeightCalculator/Tests/Program.cs
+++ BarbellWeightCalculator/Tests/Program.cs
@@ -1,9 +1,118 @@
 ﻿
+using BarbellWeightCalculator;
+using System.Diagnostics;
+
 // define the tests...
 
 List<Test> tests = new List<Test> {
-    new Test("dummy failure", () => { return false; } )
-    ,new Test("dummy success", () => { return true; } )
+
+    new Test("WeightConfiguration_TargetWeight_NotNegative", () => 
+    {
+        WeightConfiguration weightConfiguration = new (-1,0,true,new () { { 0,0 } });
+        return weightConfiguration.TargetWeight == 0;
+    }),
+
+    new Test("WeightConfiguration_TargetWeight_NotLessThanBarbellWeight", () => 
+    {
+        WeightConfiguration weightConfiguration = new (20,20,true,new () { { 0,0 } });
+        Debug.Assert(weightConfiguration.TargetWeight == 20);
+        Debug.Assert(weightConfiguration.BarbellWeight == 20);
+        weightConfiguration.TargetWeight = 10;
+        return weightConfiguration.TargetWeight == 20;
+    }),
+
+    new Test("WeightConfiguration_TargetWeight_UpperBound", () =>
+    {
+        WeightConfiguration weightConfiguration = new (WeightConfiguration.MaxTargetWeight,0,true,new () { { 0,0 } });
+        Debug.Assert(weightConfiguration.TargetWeight == WeightConfiguration.MaxTargetWeight);
+        weightConfiguration.TargetWeight =  WeightConfiguration.MaxTargetWeight + 1;
+        return weightConfiguration.TargetWeight ==  WeightConfiguration.MaxTargetWeight;
+    }),
+
+    new Test("WeightConfiguration_BarbellWeight_NotNegative", () => 
+    {
+        WeightConfiguration weightConfiguration = new (0,-10,true,new () { { 0,0 } });
+        return weightConfiguration.BarbellWeight == 0;
+    }),
+
+    new Test("WeightConfiguration_CalculateWeightSet_OnlyBarbell", () => 
+    {
+        WeightConfiguration weightConfiguration
+            = new (20,20,true, Test.Helpers.FullWeightSet);
+        return weightConfiguration.CalculatePlateSet().Count == 0;
+    }),
+
+    new Test("WeightConfiguration_CalculateWeightSet_SinglePlate", () => 
+    {
+        WeightConfiguration weightConfiguration
+            = new (70,20,true, Test.Helpers.FullWeightSet);
+        // Expect to use a single 25kg plate since we have a target weight of 70kg
+        // and the bar is 20kg, hence we need 70-20=50kg of plates, therefore 25kg
+        // per side, and we have 25kg plates, so should use that one plate per side.
+        var plateSet = weightConfiguration.CalculatePlateSet();
+        plateSet.TryGetValue(25, out uint twentyFiveCount);
+        return plateSet.Count == 1 && twentyFiveCount == 1;
+    }),
+
+    new Test("WeightConfiguration_CalculateWeightSet_TwoDistinctPlatesNoSkip", () => 
+    {
+        WeightConfiguration weightConfiguration
+            = new (110,20,true, Test.Helpers.FullWeightSet);
+        // Expect to use 1x 25kg + 1x 20kg since we have a target weight of 110kg
+        // and the bar is 20kg, hence we need 110-20=90kg of plates, therefore 45kg
+        // per side, and we have 25kg and 20kg plates, so should use one per side.
+        var plateSet = weightConfiguration.CalculatePlateSet();
+        plateSet.TryGetValue(25, out uint twentyFiveCount);
+        plateSet.TryGetValue(20, out uint twentyCount);
+        return plateSet.Count == 2 && twentyFiveCount == 1 && twentyCount == 1;
+    }),
+
+    new Test("WeightConfiguration_CalculateWeightSet_TwoDistinctPlatesWithSkip", () => 
+    {
+        WeightConfiguration weightConfiguration
+            = new (100,20,true, Test.Helpers.FullWeightSet);
+        // Expect to use 1x 25kg + 1x 15kg since we have a target weight of 100kg
+        // and the bar is 20kg, hence we need 100-20=80kg of plates, therefore 40kg
+        // per side, and we have 25kg and 15kg plates, so should use one per side.
+        // Note that the 20kg plate will be skipped.
+        var plateSet = weightConfiguration.CalculatePlateSet();
+        plateSet.TryGetValue(25, out uint twentyFiveCount);
+        plateSet.TryGetValue(15, out uint fifteenCount);
+        return plateSet.Count == 2 && twentyFiveCount == 1 && fifteenCount == 1;
+    }),
+
+    new Test("WeightConfiguration_CalculateWeightSet_CheckNotGreedy", () =>
+    {
+        WeightConfiguration weightConfiguration
+            = new (100,20,true, Test.Helpers.FullWeightSet);
+        // Expect to use 1x 25kg + 1x 15kg since we have a target weight of 100kg
+        // and the bar is 20kg, hence we need 100-20=80kg of plates, therefore 40kg
+        // per side, and we have 25kg and 15kg plates, so should use one per side.
+        // Note that the 20kg plate will be skipped.
+        var plateSet = weightConfiguration.CalculatePlateSet();
+        plateSet.TryGetValue(25, out uint twentyFiveCount);
+        plateSet.TryGetValue(15, out uint fifteenCount);
+        return plateSet.Count == 2 && twentyFiveCount == 1 && fifteenCount == 1;
+    }),
+
+    new Test("WeightConfiguration_CalculateWeightSet_CheckImpossibleInRange", () =>
+    {
+        WeightConfiguration weightConfiguration
+            = new (100.125,20,true, Test.Helpers.FullWeightSet);
+        // We don't have any .125 plates, so impossible to make this weight even
+        // though we can make weights higher than this amount
+        var plateSet = weightConfiguration.CalculatePlateSet();
+        return plateSet.Count == 0;
+    }),
+
+    new Test("WeightConfiguration_CalculateWeightSet_CheckImpossibleOutOfRange", () =>
+    {
+        WeightConfiguration weightConfiguration
+            = new ( WeightConfiguration.MaxTargetWeight,20,true, Test.Helpers.FullWeightSet);
+        // We cannot reach this value
+        var plateSet = weightConfiguration.CalculatePlateSet();
+        return plateSet.Count == 0;
+    }),
 };
 
 // then run the tests!
@@ -56,5 +165,14 @@ class Test
             NumPassed == NumRan ? ConsoleColor.Green : ConsoleColor.Red;
         Console.WriteLine($"{NumPassed}/{NumRan}");
         Console.ForegroundColor = originalColor;
+    }
+
+    public static class Helpers
+    {
+        public static SortedDictionary<double, uint> FullWeightSet
+                => new SortedDictionary<double, uint>() {
+                    { 25, 2 }, { 20, 10 }, { 15, 10 }, { 10, 2 }, { 5, 2 },
+                    { 2.5, 2 }, { 2, 2 }, { 1.5, 2 }, { 1, 2 }, { .5, 2 }
+                };
     }
 }
