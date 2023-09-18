f64457bb996faa70af51a3d823b31ca665464b42
thequinneffect <n_quinny12@hotmail.com>
2023-09-18 16:46:35 +1000

    add MVP of viewmodel
    * MainWindowViewModel is a plain old class that implements the INotifyPropertyChanged interface in order to satisfy its role as a viewmodel in MVVM arch.
    * The MainWindow view has an instance of this MainWindowViewModel set as its DataContext, which is where Bindings look for properties i.e. because we have Binding ResultText, it will look on the MainWindowViewModel for a public property called ResultText, which is present.
    * Since we implemented the notify, we can also change the ResultText, raise the event, and because the view is a listener, it will get the update. This means the view can be populated with values from the viewmodel without depending on it directly (all it cares about is it has a data context with a property called ResultText, and that it can subscribe to the PropertyChanged event of said data context).
      * The temporary update of ResultText in the contstructor demonstrates this view update via notifications

--- BarbellWeightCalculator/BarbellWeightCalculator/App.axaml.cs
+++ BarbellWeightCalculator/BarbellWeightCalculator/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace BarbellWeightCalculator
 {
@@ -15,7 +16,14 @@ namespace BarbellWeightCalculator
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow();
+                var persistenceService = new PersistenceService(
+                        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                        "barbell-weight-calculator-data.json"
+                    );
+                desktop.MainWindow = new MainWindow()
+                {
+                     DataContext = new MainWindowViewModel(persistenceService)
+                };
             }
 
             base.OnFrameworkInitializationCompleted();
---
+++ BarbellWeightCalculator/BarbellWeightCalculator/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarbellWeightCalculator
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        // I know this is a dependency on a concrete type, it's fine for this project scope
+        private PersistenceService _persistenceService;
+
+        private WeightConfiguration _weightConfiguration;
+
+        private string _resultText = "This text comes from the view model";
+        
+        public string ResultText {
+            get => _resultText;
+            set
+            {
+                _resultText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel(PersistenceService persistenceService)
+        {
+            _persistenceService = persistenceService;
+            _weightConfiguration = persistenceService.ReadWeightConfiguration();
+            Task.Run(async () =>
+            {
+                await Task.Delay(2000);
+                ResultText = $"weight config has tw={_weightConfiguration.TargetWeight}, " +
+                    $"bw={_weightConfiguration.BarbellWeight}, " +
+                    $"metric={_weightConfiguration.IsMetric}";
+            });
+            
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
